5b8ddd280906f54a0f0d8297c938c90f43ffd8b9
jts140 <77750110+jts140@users.noreply.github.com>
2021-02-03 18:08:03 -0500

    Added conditons for output

--- game.py
+++ game.py
@@ -19,23 +19,31 @@ print("You chose:", user_choice)
 
 #simulating a computer input
 foo = ["rock","paper", "scissors"]
-computer_choice = choice(foo)
+computer_choice = random.choice(foo)
 
 print("The computer chose:", computer_choice)
 
 
 print("-------------------")
 
+# determining who won
 
 
+if user_choice == computer_choice:
+        print("It's tie!")
+   elif user_choice == "paper" and computer_choice == "rock":
+        print("You win! Congrats")
+    elif user_choice == "paper" and computer_choice == "scissors":
+        print("Oh! The computer won, that's ok!")
+    elif user_choice == "rock" and computer_choice == "paper":
+        print("Oh! The computer won, that's ok!")
+    elif user_choice == "rock" and computer_choice == "scissors":
+        print("You win! Congrats")
+    elif user_choice == "scissors" and computer_choice == "paper":
+        print("You win! Congrats")
+    elif user_choice == "scissors" and computer_choice == "rock":
+        print("Oh! The computer won, that's ok!")
 
-
-
-Exit()
-
-
-
-
-print("Oh, the computer won. It's ok.")
 print("-------------------")
+
 print("Thanks for playing. Please play again!")

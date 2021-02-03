d90873408ed7811f3cf14068270d0715823e7b23
jts140 <77750110+jts140@users.noreply.github.com>
2021-02-03 17:57:04 -0500

    add comp choice

--- game.py
+++ game.py
@@ -1,36 +1,41 @@
 # game.py
+import random
 
 print("Rock, Paper, Scissors, Shoot!")
 
-
-
-
-
 print("-------------------")
+
 print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
 
 #asking user inputs
 
+print("-------------------")
+
+user_choice = input("Please choose either 'rock', 'paper', or 'scissors'")
+
+print("You chose:", user_choice)
+
+
+
+#simulating a computer input
+foo = ["rock","paper", "scissors"]
+computer_choice = choice(foo)
+
+print("The computer chose:", computer_choice)
 
 
 print("-------------------")
 
-x = input("Please choose either 'rock', 'paper', or 'scissors'")
-
-print (x)
-
-print("You chose:", x)
 
 
 
 
 
+Exit()
 
 
 
 
-print("The computer chose: 'paper'")
-print("-------------------")
 print("Oh, the computer won. It's ok.")
 print("-------------------")
 print("Thanks for playing. Please play again!")
